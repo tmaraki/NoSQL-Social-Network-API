8b416255ca17b8af92b714c5a41644b18cab3ab5
tmaraki <toshiemaraki@gmail.com>
2024-04-10 10:35:35 -0400

    finished up thoughts-controller and created routes for thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -59,9 +59,30 @@ const ThoughtController = {
 
     async createReaction(req,res) {
         try {
-            const thought = await Thought.findOneAndUpdate({_id: req.params.thoughtId});
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$addToSet: {reactions: req.body}},
+                {runValidators: true, new:true}
+                );
+                thought ? res.json(thought): res.status(404).json({message: 'Thought not found'})    
+        } catch (err) {
+            res.status(500).json({message: 'Cannot create reaction'});
+        }
+    },
+
+    async deleteReaction(req, res) {
+        try{
+            const thought = await Thought.findOneAndUpdate(
+                {_id: req.params.thoughtId},
+                {$pull: {reactions: {reactionId: req.params.reactionId}}},
+                {runValidators: true, new:true}
+            );
+
+            thought ? res.json(thought): res.status(404).json({message: 'Not Found'})
         } catch (err) {
             res.status(500).json(err);
         }
     },
-}
+};
+
+module.exports = ThoughtController
