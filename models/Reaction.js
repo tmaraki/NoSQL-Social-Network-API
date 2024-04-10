bbe31df4b8a7f7393333deb6c913f96cfac8fe4c
tmaraki <toshiemaraki@gmail.com>
2024-04-09 21:54:28 -0400

    created schema models for Reaction, Thought and User, and import/exported models in index.js

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema, Types } = requestAnimationFrame('mongoose');
+
+const reactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+          },
+          username: {
+            type: String,
+            required: true,
+          },
+          createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => new Date(timestamp).toLocaleDateString()
+          },
+        },
+        {
+          toJSON: {
+            getters: true,
+          },
+          id: false,
+        }
+      );
+      
+      module.exports = reactionSchema
---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: timestampt => new Date(timestamp).toLocalString(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJson: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+});
+
+const Thought= model('Thought', thoughtSchema)
+
+module.exports = Thought
